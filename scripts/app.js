a42531982d019a465d83b0e0f8f860b97cecbc9b
Margeaux Spring <contact@cheapwebmonkey.com>
2015-12-31 09:51:40 -0500

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+//first param is name of the app
+angular.module("todoListApp", [])
+
+  .controller('mainCtrl', function($scope) {
+  $scope.helloWorld = function(){
+    console.log("Hello there");
+  };
+})
+  .controller('coolCtrl', function($scope){
+    $scope.whoAmI = function() {
+      console.log("Hey this is the coolCtrl function");
+  };
+  $scope.helloWorld = function() {
+    console.log("This is the Hello World inside the coolCtrl, not main ctrl!");
+  }
+});;
